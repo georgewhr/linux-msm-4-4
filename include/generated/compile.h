c7d8f0f78691cf101acec292bca9d2a8f1e17bca
George Wang <georgewhr@gmail.com>
2016-10-12 17:40:24 -0700

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sun Oct 9 20:16:01 PDT 2016"
+#define LINUX_COMPILE_BY "gwang"
+#define LINUX_COMPILE_HOST "gwang-server"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
